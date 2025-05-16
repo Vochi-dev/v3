07f328d0899ac157513e48a04c5e072b6974ddef
Evgeny <evgeny.baevski@vochi.by>
2025-05-16 20:39:20 +0000

    1

--- app/services/database.py
+++ app/services/database.py
@@ -1,18 +1,23 @@
-# app/services/database.py
-
 import aiosqlite
-from app.config import DB_PATH
+import logging
 
+logger = logging.getLogger(__name__)
+DB_PATH = "/root/asterisk-webhook/asterisk_events.db"
 
 async def get_enterprises_with_tokens():
     query = """
-        SELECT number, name, bot_token, chat_id, ip, secret, host, name2
+        SELECT number, name, bot_token, chat_id, ip, secret, host, created_at, name2
         FROM enterprises
         WHERE bot_token IS NOT NULL AND bot_token != ''
     """
-
-    async with aiosqlite.connect(DB_PATH) as db:
-        db.row_factory = aiosqlite.Row
-        async with db.execute(query) as cursor:
-            rows = await cursor.fetchall()
-            return [dict(row) for row in rows]
+    try:
+        async with aiosqlite.connect(DB_PATH) as db:
+            db.row_factory = aiosqlite.Row
+            async with db.execute(query) as cursor:
+                rows = await cursor.fetchall()
+                enterprises = [dict(row) for row in rows]
+                logger.debug(f"🔄 Загружено {len(enterprises)} предприятий с токенами.")
+                return enterprises
+    except Exception as e:
+        logger.exception(f"❌ Ошибка при получении предприятий с токенами: {e}")
+        return []
